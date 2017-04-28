581bfadf75105b3873f732f0fc7a7461f2e08141
hjmccain <hannahjmccain@gmail.com>
2017-04-28 15:22:02 -0400

    Add proxy server.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
